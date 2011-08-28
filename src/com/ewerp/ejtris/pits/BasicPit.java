45f295f471f05c7573276e8723729ef9d1148c50
Curtis Boyden <cboyden@ewerp.com>
2011-08-27 22:39:00 -0400

    Added horizontal quick movement

--- src/com/ewerp/ejtris/pits/BasicPit.java
+++ src/com/ewerp/ejtris/pits/BasicPit.java
@@ -18,146 +18,168 @@ import com.ewerp.ejtris.shapes.ShapeHelper;
 public class BasicPit {
     private int pitX = 200;
     private int pitY = 15;
-    
+
     private int width = 20;
     private int height = 28;
-    
+
     private long lastTick = 0;
-    
+    private long lastMoveLeftTick = 0;
+    private long lastMoveRightTick = 0;
+
     protected IShape inactiveShape;
     protected final byte[][] inactiveShapeDefinition;
-    
+
     protected IShape activeShape;
-    
+
     public BasicPit() throws SlickException {
         inactiveShapeDefinition = new byte[height][width];
-        for(int y = 0; y < height; y++) {
-            Arrays.fill(inactiveShapeDefinition[y], (byte)'O');
+        for (int y = 0; y < height; y++) {
+            Arrays.fill(inactiveShapeDefinition[y], (byte) 'O');
         }
-        
-        inactiveShape = new AbstractShape(inactiveShapeDefinition) {};
-        
+
+        inactiveShape = new AbstractShape(inactiveShapeDefinition) {
+        };
+
         activeShape = ShapeHelper.generateShape();
         lastTick = System.currentTimeMillis();
     }
-    
+
     public void render(GameContainer gameContainer, StateBasedGame game, Graphics graphicsContext) {
         inactiveShape.draw(graphicsContext, pitX, pitY);
         activeShape.draw(graphicsContext, pitX + (activeShape.getLeft() * ShapeHelper.BLOCK_WIDTH), pitY + (activeShape.getTop() * ShapeHelper.BLOCK_HEIGHT));
     }
-    
+
     public void update(GameContainer gameContainer, StateBasedGame game, int elapsedTime) {
         long currentTime = System.currentTimeMillis();
         boolean moveDown = false;
-        
+        boolean moveLeft = false;
+        boolean moveRight = false;
+
         Input input = gameContainer.getInput();
-       
-       if(input.isKeyDown(Input.KEY_DOWN)) {
-           if(currentTime - lastTick > 100) {
-               moveDown = true;
-           }
-       }
-        
-       if (input.isKeyPressed(Input.KEY_DOWN)) {
+
+        if (input.isKeyDown(Input.KEY_DOWN)) {
+            if (currentTime - lastTick > 100) {
+                moveDown = true;
+            }
+        } else if(input.isKeyDown(Input.KEY_LEFT)) {
+            if (currentTime - lastMoveLeftTick > 200) {
+                moveLeft = true;
+            }
+        } else if(input.isKeyDown(Input.KEY_RIGHT)) {
+            if (currentTime - lastMoveRightTick > 200) {
+                moveRight = true;
+            }
+        }
+
+        if (input.isKeyPressed(Input.KEY_DOWN)) {
             // Move the block down
             moveDown = true;
         } else if (input.isKeyPressed(Input.KEY_SPACE)) {
             activeShape.rotate();
         } else if (input.isKeyPressed(Input.KEY_LEFT)) {
             // Move the block left
-            if(activeShape.getLeft() > 0) {
+            moveLeft = true;
+        } else if (input.isKeyPressed(Input.KEY_RIGHT)) {
+            // Move the block right
+            moveRight = true;
+        }
+
+        if (moveLeft) {
+            if (activeShape.getLeft() > 0) {
                 activeShape.setLeft(activeShape.getLeft() - 1);
-                if(isCollision()) {
+                if (isCollision()) {
                     // undo the move, it is not valid
                     activeShape.setLeft(activeShape.getLeft() + 1);
                 }
             }
-        } else if (input.isKeyPressed(Input.KEY_RIGHT)) {
-            // Move the block right
-            if(activeShape.getLeft() + activeShape.getWidth() < width) {
+            lastMoveLeftTick = currentTime;
+        } else if (moveRight) {
+            if (activeShape.getLeft() + activeShape.getWidth() < width) {
                 activeShape.setLeft(activeShape.getLeft() + 1);
-                if(isCollision()) {
+                if (isCollision()) {
                     // undo the move, it is not valid
                     activeShape.setLeft(activeShape.getLeft() - 1);
                 }
             }
+            lastMoveRightTick = currentTime;
         }
-        
-        if(moveDown || currentTime - lastTick > 1000) {
+
+        if (moveDown || currentTime - lastTick > 1000) {
             // Move block down
             activeShape.setTop(activeShape.getTop() + 1);
             lastTick = currentTime;
-            
-            if(isCollision()) {
+
+            if (isCollision()) {
                 // Create a new block
                 try {
                     // Back it up to stack
                     activeShape.setTop(activeShape.getTop() - 1);
                     mergeActiveShape();
-                    
+
                     // TODO: Test line building
                     lineTest();
 
                     activeShape = ShapeHelper.generateShape();
-                    
-                    if(isCollision()) {
+
+                    if (isCollision()) {
                         // TODO: GAME OVER!!!
                         System.out.println("Game over");
                     }
-                    
+
                 } catch (SlickException e) {
                     e.printStackTrace();
                 }
             }
         }
     }
-    
+
     protected int getHeight() {
         return height;
     }
-    
+
     protected boolean isCollision() {
-        if(activeShape.getBottom() >= getHeight()) { 
+        if (activeShape.getBottom() >= getHeight()) {
             return true;
         }
-        
-        if(activeShape.isCollision(inactiveShape)) {
+
+        if (activeShape.isCollision(inactiveShape)) {
             return true;
         }
-        
+
         return false;
     }
-    
+
     /**
      * Merge the active shape into the inactive shape
      */
     protected void mergeActiveShape() throws SlickException {
-        for(Point p : activeShape.getTranslatedPoints()) {
+        for (Point p : activeShape.getTranslatedPoints()) {
             inactiveShapeDefinition[p.y][p.x] = 'X';
         }
-        
-        inactiveShape = new AbstractShape(inactiveShapeDefinition) {};
+
+        inactiveShape = new AbstractShape(inactiveShapeDefinition) {
+        };
     }
-    
+
     protected void lineTest() {
         List<Integer> lines = new ArrayList<Integer>();
-        
-        for(int y = 0; y < inactiveShapeDefinition.length; y++) {
-            for(int x = 0; x <= inactiveShapeDefinition[y].length; x++) {
-                if(x == inactiveShapeDefinition[y].length) {
+
+        for (int y = 0; y < inactiveShapeDefinition.length; y++) {
+            for (int x = 0; x <= inactiveShapeDefinition[y].length; x++) {
+                if (x == inactiveShapeDefinition[y].length) {
                     // We have a complete line, add it to the list
                     lines.add(y);
                     break;
                 }
-                if(inactiveShapeDefinition[y][x] != 'X') {
+                if (inactiveShapeDefinition[y][x] != 'X') {
                     break;
                 }
             }
         }
-        
-        if(lines.size() > 0) {
+
+        if (lines.size() > 0) {
             // We have matching lines, do something!
-            for(Integer i : lines) {
+            for (Integer i : lines) {
                 System.out.println("Line found: " + i);
             }
         }
