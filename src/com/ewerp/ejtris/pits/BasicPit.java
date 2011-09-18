bc3e6c2069820eb67f64a2bbe45fdf1f362f95bc
Curtis Boyden <cboyden@ewerp.com>
2011-09-17 23:17:42 -0400

    Added line removal support

--- src/com/ewerp/ejtris/pits/BasicPit.java
+++ src/com/ewerp/ejtris/pits/BasicPit.java
@@ -3,6 +3,7 @@ package com.ewerp.ejtris.pits;
 import java.awt.Point;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import org.newdawn.slick.GameContainer;
@@ -17,7 +18,8 @@ import com.ewerp.ejtris.shapes.AbstractShape;
 import com.ewerp.ejtris.shapes.IShape;
 import com.ewerp.ejtris.shapes.ShapeHelper;
 
-public class BasicPit {
+public class BasicPit implements ILineListener {
+
     private int pitX = 200;
     private int pitY = 15;
 
@@ -28,11 +30,13 @@ public class BasicPit {
     private long lastMoveLeftTick = 0;
     private long lastMoveRightTick = 0;
 
+    private List<ILineListener> lineListeners = new ArrayList<ILineListener>();
+
     protected IShape inactiveShape;
     protected final byte[][] inactiveShapeDefinition;
 
     protected IShape activeShape;
-    
+
     protected IEJTrisController controller;
 
     public BasicPit(IEJTrisController ejTrisController) throws SlickException {
@@ -40,16 +44,19 @@ public class BasicPit {
         controller = ejTrisController;
         initialize();
     }
-    
+
     protected void initialize() throws SlickException {
         for (int y = 0; y < height; y++) {
             Arrays.fill(inactiveShapeDefinition[y], (byte) 'O');
         }
 
-        inactiveShape = new AbstractShape(inactiveShapeDefinition) {};
+        inactiveShape = new AbstractShape(inactiveShapeDefinition) {
+        };
 
         activeShape = ShapeHelper.generateShape();
         lastTick = System.currentTimeMillis();
+
+        registerLineListener(this);
     }
 
     public void render(GameContainer gameContainer, StateBasedGame game, Graphics graphicsContext) {
@@ -69,11 +76,11 @@ public class BasicPit {
             if (currentTime - lastTick > 100) {
                 moveDown = true;
             }
-        } else if(input.isKeyDown(Input.KEY_LEFT)) {
+        } else if (input.isKeyDown(Input.KEY_LEFT)) {
             if (currentTime - lastMoveLeftTick > 200) {
                 moveLeft = true;
             }
-        } else if(input.isKeyDown(Input.KEY_RIGHT)) {
+        } else if (input.isKeyDown(Input.KEY_RIGHT)) {
             if (currentTime - lastMoveRightTick > 200) {
                 moveRight = true;
             }
@@ -84,7 +91,7 @@ public class BasicPit {
             moveDown = true;
         } else if (input.isKeyPressed(Input.KEY_SPACE)) {
             activeShape.rotateClockwise();
-            if(isCollision()) {
+            if (isCollision()) {
                 // Undo the rotation, we just rotated into another shape
                 activeShape.rotateCounterClockwise();
             }
@@ -133,7 +140,6 @@ public class BasicPit {
                     activeShape = ShapeHelper.generateShape();
 
                     if (isCollision()) {
-                        // TODO: GAME OVER!!!
                         // Take a screenshot
                         Image screenshot = new Image(gameContainer.getWidth(), gameContainer.getHeight());
                         gameContainer.getGraphics().copyArea(screenshot, 0, 0);
@@ -195,9 +201,45 @@ public class BasicPit {
 
         if (lines.size() > 0) {
             // We have matching lines, do something!
-            for (Integer i : lines) {
-                System.out.println("Line found: " + i);
+            notifyLineListeners(lines);
+        }
+    }
+
+    public void notify(List<Integer> lines) {
+        try {
+            // Remove lines
+            for (Integer line : lines) {
+                for (int x = 0; x < width; x++) {
+                    inactiveShapeDefinition[line][x] = 'O';
+                }
             }
+
+            // Shift all rows down
+            Collections.sort(lines);
+
+            // For each empty line, shift all lines above it down one row
+            for (Integer line : lines) {
+                for (int y = line; y > 0; y--) {
+                    inactiveShapeDefinition[y] = inactiveShapeDefinition[y - 1];
+                }
+            }
+
+            inactiveShape = new AbstractShape(inactiveShapeDefinition) {
+            };
+
+        } catch (SlickException e) {
+            // TODO: Log error
+            e.printStackTrace();
+        }
+    }
+
+    public void registerLineListener(ILineListener lineListener) {
+        lineListeners.add(lineListener);
+    }
+
+    protected void notifyLineListeners(List<Integer> lines) {
+        for (ILineListener lineListener : lineListeners) {
+            lineListener.notify(lines);
         }
     }
 }
---
+++ src/com/ewerp/ejtris/pits/ILineListener.java
@@ -0,0 +1,7 @@
+package com.ewerp.ejtris.pits;
+
+import java.util.List;
+
+public interface ILineListener {
+    public void notify(List<Integer> lines);
+}
